9b21990ac48b90cac20acedcc01fa7e1b9458851
Your Name <ksushasimonenko52@gmail.com>
2025-05-22 20:14:38 +0300

    Опис змін

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const videos = [
-    'img/videos/video1.mp4',
-    'img/videos/video3.mp4',
-    'img/videos/video2.mp4'
+    'assets/videos/video1.mp4',
+    'assets/videos/video3.mp4',
+    'assets/videos/video2.mp4'
 ];
 
 let currentIndex = 0;
